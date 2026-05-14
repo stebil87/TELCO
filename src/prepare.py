ef4acdc926d688aa60fcb8f250c48c5f86540b32
Billeter Stefano <stefano.billeter@student.supsi.ch>
2026-05-14 17:31:01 +0200

    pipeline script test

--- src/prepare.py
+++ src/prepare.py
@@ -4,6 +4,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 import joblib
+import warnings
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 os.makedirs("data/processed", exist_ok=True)
 os.makedirs("models", exist_ok=True)
